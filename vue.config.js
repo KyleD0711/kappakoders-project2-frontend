074d76cc70fd7c26a4a827a44eba1e23bd1f7359
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-10-01 14:16:42 -0500

    Changed config file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: process.env.APP_ENV === "development" ? "/" : "/2024/project2/t1",
+  transpileDependencies: true,
+});
