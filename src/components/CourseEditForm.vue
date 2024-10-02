ac69367067bbb4a738843b9e5575317ebb8b4248
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-10-02 11:57:37 -0500

    reformatted course edit form

--- src/components/CourseEditForm.vue
+++ src/components/CourseEditForm.vue
@@ -1,36 +1,33 @@
 <script setup>
 import { ref, defineProps, onMounted } from "vue";
-import router from '@/router.js'
-
+import router from "@/router.js";
 
 import courseServices from "@/services/courseServices";
 
-
 const props = defineProps({
-    id: {
-        type: Number,
-        required: false,
-        default: -1
-    }
-})
-
+  id: {
+    type: Number,
+    required: false,
+    default: -1,
+  },
+});
 
 const editData = ref({
-    dept: '',
-    courseNumber: '',
-    level: '',
-    hours: '',
-    name: '',
-    desc: '',
+  dept: "",
+  courseNumber: "",
+  level: "",
+  hours: "",
+  name: "",
+  desc: "",
 });
 
 onMounted(async () => {
-    if (props.id !== -1) {
+  if (props.id !== -1) {
     try {
       const response = await courseServices.getCourseById(props.id);
       const course = response.data;
 
-        console.log(response.data);
+      console.log(response.data);
 
       // Populate editData with the course information
       editData.value = {
@@ -51,53 +48,58 @@ const submitForm = () => {
   // Logic to handle form submission, e.g., update or save course
   console.log("Form submitted with:", editData.value);
 
-  if (editData.value.dept == '' || editData.value.courseNumber == '' || editData.value.level == '' || editData.value.hours == '' || editData.value.name == '') {
-
+  if (
+    editData.value.dept == "" ||
+    editData.value.courseNumber == "" ||
+    editData.value.level == "" ||
+    editData.value.hours == "" ||
+    editData.value.name == ""
+  ) {
     alert("Please input all required fields");
   } else {
+    if (props.id !== -1) {
+      courseServices
+        .updateCourse(props.id, editData.value)
+        .then(() => {
+          console.log("Course updated successfully");
+        })
+        .catch((error) => {
+          console.error("Error updating course:", error);
+        });
+    } else {
+      courseServices
+        .createCourse(editData.value)
+        .then(() => {
+          console.log("Course updated successfully");
+        })
+        .catch((error) => {
+          console.error("Error updating course:", error);
+        });
+    }
 
-  if (props.id !== -1) {
-    courseServices.updateCourse(props.id, editData.value)
-        .then(() => {
-        console.log("Course updated successfully");
-        })
-        .catch((error) => {
-        console.error("Error updating course:", error);
-        });
-  } else {
-    courseServices.createCourse(editData.value)
-        .then(() => {
-        console.log("Course updated successfully");
-        })
-        .catch((error) => {
-        console.error("Error updating course:", error);
-        });
+    router.push({
+      name: "home-page",
+    });
   }
-
-  router.push({
-    name: 'home-page',
-  });
-}
 };
 
 const goBack = () => {
-    router.push({
-    name: 'home-page',
+  router.push({
+    name: "home-page",
   });
-}
-
+};
 </script>
 
 <template>
   <v-form @submit.prevent="submitForm">
-    <div class="field-name">Department * </div>
+    <div class="field-name">Department:</div>
     <v-text-field
       label="Enter Department"
       v-model="editData.dept"
       outlined
     ></v-text-field>
 
-    <div class="field-name">Course Number * </div>
+    <div class="field-name">Course Number:</div>
     <v-text-field
       label="Enter Course Number"
       v-model="editData.courseNumber"
@@ -105,7 +107,7 @@ const goBack = () => {
       outlined
     ></v-text-field>
 
-    <div class="field-name">Course Level * </div>
+    <div class="field-name">Course Level:</div>
     <v-text-field
       label="Enter Course Level"
       v-model="editData.level"
@@ -113,7 +115,7 @@ const goBack = () => {
       outlined
     ></v-text-field>
 
-    <div class="field-name">Course Hours * </div>
+    <div class="field-name">Course Hours:</div>
     <v-text-field
       label="Enter Course Hours"
       v-model="editData.hours"
@@ -121,7 +123,7 @@ const goBack = () => {
       outlined
     ></v-text-field>
 
-    <div class="field-name">Course Name * </div>
+    <div class="field-name">Course Name:</div>
     <v-text-field
       label="Enter Course Name"
       v-model="editData.name"
@@ -129,7 +131,7 @@ const goBack = () => {
       outlined
     ></v-text-field>
 
-    <div class="field-name">Course Description: </div>
+    <div class="field-name">Course Description:</div>
     <v-text-field
       label="Enter Course Description"
       v-model="editData.description"
@@ -137,27 +139,27 @@ const goBack = () => {
       outlined
     ></v-text-field>
 
-    <v-btn class="cancel" color="primary" type="cancel" @click="goBack()">Cancel</v-btn>
+    <v-btn class="cancel" color="primary" type="cancel" @click="goBack()"
+      >Cancel</v-btn
+    >
     <v-btn class="sub-btn" color="primary" type="submit">Submit</v-btn>
   </v-form>
 </template>
 
 <style scoped>
-
 .v-form {
-    margin-left: 5%;
-    margin-right: 5%;
-    margin-top: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
+  margin-top: 5%;
 }
 
 .v-btn {
-    width: 40%;
-    margin-left: 5%;
-    margin-right: 5%;
+  width: 40%;
+  margin-left: 5%;
+  margin-right: 5%;
 }
 
 .cancel {
-background-color: #7d2534 !important;
+  background-color: #7d2534 !important;
 }
-
 </style>
